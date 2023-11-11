f673e2b7d16e28359fde40882e28a4ab4bc0e70a
shawnm4 <shawnm18057310@gmail.com>
2023-11-11 16:47:52 -0500

    html and css only

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="calculator-container">
+        <div className="input-container">
+          <input className="num-input" type="number"></input>
+        </div>
+        <div className="func-container">
+          <div className="nums-container">
+            <div className="numbtn btn">1</div>
+            <div className="numbtn btn">2</div>
+            <div className="numbtn btn">3</div>
+            <div className="numbtn btn">4</div>
+            <div className="numbtn btn">5</div>
+            <div className="numbtn btn">6</div>
+            <div className="numbtn btn">7</div>
+            <div className="numbtn btn">8</div>
+            <div className="numbtn btn">9</div>
+            <div className="numbtn btn">0</div>
+            <div className="funcbtn btn">+</div>
+            <div className="funcbtn btn">-</div>
+            <div className="funcbtn btn">÷</div>
+            <div className="funcbtn btn">x</div>
+            <div className="funcbtn btn">C</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
