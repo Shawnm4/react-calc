822c3f2423707fa88a1bde1434b649a3c4c18219
shawnm4 <shawnm18057310@gmail.com>
2023-11-12 17:09:20 -0500

    added a little functionality

--- src/App.js
+++ src/App.js
@@ -1,33 +1,56 @@
 // import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 
+const nums = [
+  { id: 1 },
+  { id: 2 },
+  { id: 3 },
+  { id: 4 },
+  { id: 5 },
+  { id: 6 },
+  { id: 7 },
+  { id: 8 },
+  { id: 9 },
+  { id: 0 },
+];
+
 function App() {
+  const [acc, setAccu] = useState("");
+  function displayNums(num) {
+    setAccu((acc) => num.toString());
+    console.log(acc);
+  }
   return (
     <div className="app-container">
       <div className="calculator-container">
         <div className="input-container">
-          <input className="num-input" type="number"></input>
+          <div className="num-display">{acc}</div>
         </div>
         <div className="func-container">
           <div className="nums-container">
-            <div className="numbtn btn">1</div>
-            <div className="numbtn btn">2</div>
-            <div className="numbtn btn">3</div>
-            <div className="numbtn btn">4</div>
-            <div className="numbtn btn">5</div>
-            <div className="numbtn btn">6</div>
-            <div className="numbtn btn">7</div>
-            <div className="numbtn btn">8</div>
-            <div className="numbtn btn">9</div>
-            <div className="numbtn btn">0</div>
-            <div className="funcbtn btn">+</div>
+            {nums.map((obj) => (
+              <RenderNums
+                nums={obj.id}
+                key={obj.id}
+                onHandleDisplay={displayNums}
+              />
+            ))}
             <div className="funcbtn btn">-</div>
             <div className="funcbtn btn">÷</div>
             <div className="funcbtn btn">x</div>
-            <div className="funcbtn btn">C</div>
+            <div className="funcbtn btn clearbtn">C</div>
           </div>
         </div>
       </div>
+    </div>
+  );
+}
+
+function RenderNums({ nums, onHandleDisplay }) {
+  return (
+    <div onClick={() => onHandleDisplay(nums)} className="btn numbtn">
+      {nums}
     </div>
   );
 }
